83fd2533b19e31a685d3a479ee0fb5779a3e1a77
rajil <rajil@apples-MacBook-Air-3.local>
2019-06-04 00:43:34 +0530

    magic comment frozen_string_literal added

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'set'
 require_relative 'dictionary'
 require_relative 'digit_char_array'
--- lib/digit_char_array.rb
+++ lib/digit_char_array.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module DigitCharArray
   def self.fetch
     {
