f1c3fedd3e017a7265bc4a2c8c89fdce38114392
rajil <rajil@apples-MacBook-Air-3.local>
2019-06-08 16:40:22 +0530

    rubocop fixes

--- lib/converter.rb
+++ lib/converter.rb
@@ -5,25 +5,23 @@ require_relative 'dictionary'
 
 # Convert phone number to meaningfull word(s) based on available dictionary.
 class Converter
-
-  INVALID_DIGITS = %w(0 1)
+  INVALID_DIGITS = %w[0 1].freeze
   TWO_WORDS_SET  = 5
-  THREE_LETTER_COMBINATION  = [3, 3, 4]
+  THREE_LETTER_COMBINATION = [3, 3, 4].freeze
   DIGIT_CHAR_MAP = {
-                      '2' => %w(a b c),
-                      '3' => %w(d e f),
-                      '4' => %w(g h i),
-                      '5' => %w(j k l),
-                      '6' => %w(m n o),
-                      '7' => %w(p q r s),
-                      '8' => %w(t u v),
-                      '9' => %w(w x y z)
-                  }
+    '2' => %w[a b c],
+    '3' => %w[d e f],
+    '4' => %w[g h i],
+    '5' => %w[j k l],
+    '6' => %w[m n o],
+    '7' => %w[p q r s],
+    '8' => %w[t u v],
+    '9' => %w[w x y z]
+  }.freeze
 
   attr_accessor :phone_number,
                 :phone_number_words
   attr_reader   :dictionary
-
 
   def initialize(phone_number)
     @phone_number       = phone_number
@@ -33,12 +31,13 @@ class Converter
 
   def to_words
     return 'Invalid phone number!' unless phone_number_valid?
+
     generate_words
   end
 
   # Validate the phone number
   def phone_number_valid?
-    !phone_number.nil? && phone_number.to_s.length ==10 && !contains_one_zero?
+    !phone_number.nil? && phone_number.to_s.length == 10 && !contains_one_zero?
   end
 
   private
@@ -46,25 +45,25 @@ class Converter
   def generate_words
     two_word_combinations   = []
     three_word_combinations = []
-    #this will find all the one word combinations
+    # this will find all the one word combinations
     one_word_combinations = select_meaningful_words(phone_number_chars).to_a
 
-    #this will find all the two words combinations
+    # this will find all the two words combinations
     # 3+7, 4+6, 5+5, 6+4, 7+3, 10, 3+3+4, 3+4+3, 4+3+3
     TWO_WORDS_SET.times do |index|
-      first_word  = select_meaningful_words(phone_number_chars[0..2+index]).to_a
-      second_word = select_meaningful_words(phone_number_chars[3+index..9]).to_a
+      first_word  = select_meaningful_words(phone_number_chars[0..2 + index]).to_a
+      second_word = select_meaningful_words(phone_number_chars[3 + index..9]).to_a
       two_word_combinations << first_word.product(second_word)
     end
     two_word_combinations = two_word_combinations.flatten(1)
 
-    #this will find all the three words combinations
+    # this will find all the three words combinations
     # 3+3+4, 3+4+3, 4+3+3
     THREE_LETTER_COMBINATION.permutation.to_a.uniq.each do |combination|
       words = []
       combination.each_with_index do |_, index|
-        starting_index = combination.slice(0,index).inject(0, :+)
-        ending_index   = combination.slice(0,index+1).inject(0, :+) - 1
+        starting_index = combination.slice(0, index).inject(0, :+)
+        ending_index   = combination.slice(0, index + 1).inject(0, :+) - 1
         words << select_meaningful_words(phone_number_chars[starting_index..ending_index]).to_a
       end
       three_word_combinations += words[0].product(words[1], words[2])
@@ -87,7 +86,7 @@ class Converter
   end
 
   def phone_number_chars
-    phone_number_array.map{|digit|DIGIT_CHAR_MAP[digit]}
+    phone_number_array.map { |digit| DIGIT_CHAR_MAP[digit] }
   end
 
   def phone_number_array
@@ -97,5 +96,4 @@ class Converter
   def contains_one_zero?
     !(invalid_digits & phone_number_array).empty?
   end
-
 end
--- lib/phone_number_to_words.rb
+++ lib/phone_number_to_words.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'pry'
 require 'benchmark'
 require 'prettyprint'
