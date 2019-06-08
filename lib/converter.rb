eeda8b8724cea99ff1fba6db6aad8a9c171c776b
rajil <rajil@apples-MacBook-Air-3.local>
2019-06-08 16:08:24 +0530

    replaced repeatative three words combination code by a loop

--- lib/converter.rb
+++ lib/converter.rb
@@ -8,6 +8,7 @@ class Converter
 
   INVALID_DIGITS = %w(0 1)
   TWO_WORDS_SET  = 5
+  THREE_LETTER_COMBINATION  = [3, 3, 4]
   attr_accessor :phone_number,
                 :phone_number_words
   attr_reader   :digit_char_map,
@@ -46,20 +47,15 @@ class Converter
     end
     two_word_combinations = two_word_combinations.flatten(1)
 
-    first_word  = select_meaningful_words(phone_number_chars[0..2]).to_a
-    second_word = select_meaningful_words(phone_number_chars[3..5]).to_a
-    third_word  = select_meaningful_words(phone_number_chars[6..9]).to_a
-    three_word_combinations = first_word.product(second_word, third_word)
-
-    first_word  = select_meaningful_words(phone_number_chars[0..2]).to_a
-    second_word = select_meaningful_words(phone_number_chars[3..6]).to_a
-    third_word  = select_meaningful_words(phone_number_chars[7..9]).to_a
-    three_word_combinations += first_word.product(second_word, third_word)
-
-    first_word  = select_meaningful_words(phone_number_chars[0..3]).to_a
-    second_word = select_meaningful_words(phone_number_chars[4..6]).to_a
-    third_word  = select_meaningful_words(phone_number_chars[7..9]).to_a
-    three_word_combinations += first_word.product(second_word, third_word)
+    THREE_LETTER_COMBINATION.permutation.to_a.uniq.each do |combination|
+      words = []
+      combination.each_with_index do |_, index|
+        starting_index = combination.slice(0,index).inject(0, :+)
+        ending_index   = combination.slice(0,index+1).inject(0, :+) - 1
+        words << select_meaningful_words(phone_number_chars[starting_index..ending_index]).to_a
+      end
+      three_word_combinations += words[0].product(words[1], words[2])
+    end
 
     @phone_number_words += one_word_combinations + two_word_combinations + three_word_combinations
     @phone_number_words
